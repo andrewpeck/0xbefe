0c6c8337268e93c4504a72db7b5da7451f0ae94a
Evaldas Juska <evka85@gmail.com>
2020-12-18 00:17:41 +0200

    added apex me0 flavor, plus a few minor tweaks

---
+++ Top/me0_apex/me0_apex.tcl
@@ -0,0 +1,16 @@
+#vivado
+
+set FPGA xcku15p-ffva1760-2-e
+
+set DESIGN    "[file rootname [file tail [info script]]]"
+set PATH_REPO "[file normalize [file dirname [info script]]]/../../"
+source $PATH_REPO/Top/common.tcl
+
+source $PATH_REPO/Hog/Tcl/create_project.tcl
+
+set_property default_lib work [current_project]
+set_property top gem_apex [current_fileset]
+
+set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs impl_1]
+set_property AUTO_INCREMENTAL_CHECKPOINT 1 [get_runs synth_1]
+
