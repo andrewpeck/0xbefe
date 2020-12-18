f2d36a8d27a7426f1d2a16a750eaf92921ee9c44
Evaldas Juska <evka85@gmail.com>
2020-12-18 03:06:49 +0200

    CI conf added for ge21_apex, me0_apex, me_cvp13

--- Top/me0_cvp13/me0_cvp13.tcl
+++ Top/me0_cvp13/me0_cvp13.tcl
@@ -4,6 +4,17 @@ set FPGA xcvu13p-figd2104-2-e
 
 set DESIGN    "[file rootname [file tail [info script]]]"
 set PATH_REPO "[file normalize [file dirname [info script]]]/../../"
+
+#https://www.xilinx.com/support/answers/72570.html
+
+set PYTHONPATH $::env(PYTHONPATH)
+set PYTHONHOME $::env(PYTHONHOME)
+unset env(PYTHONPATH)
+unset env(PYTHONHOME)
+puts [exec bash -c {cd ../.. && make update_me0_cvp13}]
+set env(PYTHONPATH) $PYTHONPATH
+set env(PYTHONHOME) $PYTHONHOME
+
 source $PATH_REPO/Top/common.tcl
 
 source $PATH_REPO/Hog/Tcl/create_project.tcl
