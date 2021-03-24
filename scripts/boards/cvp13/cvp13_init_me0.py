05ee4fe0db93a1dcd5c2d258c4828449e580bc4e
Abhisek Datta <abhisek.datta@cern.ch>
2021-03-24 15:02:10 -0700

    change init procedure for ME0 - reset MGT links order

--- scripts/boards/cvp13/cvp13_init_me0.py
+++ scripts/boards/cvp13/cvp13_init_me0.py
@@ -15,7 +15,13 @@ def main():
         writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_DIFF_CTRL" % chan), 0x18)
         writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_POLARITY" % chan), 0)
         writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.RX_POLARITY" % chan), 0)
-        writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
+        if chan%2==0:
+	    writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
+
+    sleep(2)
+    for chan in range(16):
+	if chan%2!=0:
+	     writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
 
     sleep(0.1)
     writeReg(getNode("GEM_AMC.GEM_SYSTEM.CTRL.GLOBAL_RESET"), 1)
