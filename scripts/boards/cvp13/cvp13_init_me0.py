3b4aaa4f1d587bda954865ce93592e082ce9575f
Abhisek Datta <abhisek.datta@cern.ch>
2021-03-26 12:06:34 -0700

    disable watchdog for lpgbts before reset

--- scripts/boards/cvp13/cvp13_init_me0.py
+++ scripts/boards/cvp13/cvp13_init_me0.py
@@ -11,20 +11,39 @@ def main():
 
     writeReg(getNode("GEM_AMC.TTC.CTRL.DISABLE_PHASE_ALIGNMENT"), 1)
 
+    ### To prevent 0's getting written to slave lpGBT over EC link and prevent slave lpGBT locking to some internal clock
+    # Disabling watchdog for master and slave lpGBTs
+    writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.GBTX_I2C_ADDR"), 0x70)
+    writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.READ_WRITE_LENGTH"), 1)
+    for chan in range(16):
+        writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.GBTX_LINK_SELECT"), chan)
+        writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.ADDRESS"), 0xED)
+        writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.WRITE_DATA"), 0x63)
+        writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.EXECUTE_WRITE"), 1)
+    sleep(0.1)
+
     for chan in range(16):
         writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_DIFF_CTRL" % chan), 0x18)
         writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_POLARITY" % chan), 0)
         writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.RX_POLARITY" % chan), 0)
-        # Only reset master lpGBTs
-	if chan%2==0:
-	    writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
+        # Only reset master lpGBTs first (to be able to bring slave lpGBTs to READY state after their reset)
+        if chan%2==0:
+            writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
 
     # Sleep before resetting slave lpGBTs
     sleep(2)
     for chan in range(16):
-	# Only reset slave lpGBTs
-	if chan%2!=0:
-	     writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
+        # Only reset slave lpGBTs
+        if chan%2!=0:
+            writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
+
+    sleep(0.1)
+    # Enabling watchdog for master and slave lpGBTs
+    for chan in range(16):
+        writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.GBTX_LINK_SELECT"), chan)
+        writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.ADDRESS"), 0xED)
+        writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.WRITE_DATA"), 0x03)
+        writeReg(getNode("GEM_AMC.SLOW_CONTROL.IC.EXECUTE_WRITE"), 1)
 
     sleep(0.1)
     writeReg(getNode("GEM_AMC.GEM_SYSTEM.CTRL.GLOBAL_RESET"), 1)
