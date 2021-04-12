f98b0edd9f6c3b6f48e17e89938f5520f70bada0
Abhisek Datta <abhisek.datta@cern.ch>
2021-04-12 11:12:22 -0700

    add back sleep between MGT channel resets (need in I2C mode for lpGBT)

--- scripts/boards/cvp13/cvp13_init_me0.py
+++ scripts/boards/cvp13/cvp13_init_me0.py
@@ -27,7 +27,15 @@ def main():
         writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_DIFF_CTRL" % chan), 0x18)
         writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_POLARITY" % chan), 0)
         writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.RX_POLARITY" % chan), 0)
-        writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
+        # Only reset MGT channel for master lpGBTs
+        if chan%2==0:
+            writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
+
+    # Sleep and thhen reset MGT channel for slave lpGBTs (so that master lpGBTs are already ready)
+    sleep(2)
+    for chan in range(16):
+        if chan%2!=0:
+            writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
 
     sleep(0.1)
     # Enabling watchdog for master and slave lpGBTs
