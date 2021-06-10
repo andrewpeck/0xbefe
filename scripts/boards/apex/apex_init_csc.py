32eec7ebadd13a50f1307801e19cad02231f02aa
Evaldas Juska <evaldas.juska@cern.ch>
2021-06-10 17:19:09 +0300

    fix CSC init scripts (replace GEM_AMC with CSC_FED)

--- scripts/boards/apex/apex_init_csc.py
+++ scripts/boards/apex/apex_init_csc.py
@@ -12,19 +12,19 @@ def main():
 
     parseXML()
 
-    writeReg(getNode("GEM_AMC.TTC.CTRL.DISABLE_PHASE_ALIGNMENT"), 1)
+    writeReg(getNode("CSC_FED.TTC.CTRL.DISABLE_PHASE_ALIGNMENT"), 1)
 
     for chan in range(4):
-        writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_DIFF_CTRL" % chan), 0x18)
-        writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_POLARITY" % chan), TX_INVERTION[chan])
-        writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.RX_POLARITY" % chan), RX_INVERTION[chan])
-        writeReg(getNode("GEM_AMC.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
+        writeReg(getNode("CSC_FED.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_DIFF_CTRL" % chan), 0x18)
+        writeReg(getNode("CSC_FED.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.TX_POLARITY" % chan), TX_INVERTION[chan])
+        writeReg(getNode("CSC_FED.OPTICAL_LINKS.MGT_CHANNEL_%d.CTRL.RX_POLARITY" % chan), RX_INVERTION[chan])
+        writeReg(getNode("CSC_FED.OPTICAL_LINKS.MGT_CHANNEL_%d.RESET" % chan), 1)
 
     sleep(0.1)
-    writeReg(getNode("GEM_AMC.GEM_SYSTEM.CTRL.GLOBAL_RESET"), 1)
+    writeReg(getNode("CSC_FED.GEM_SYSTEM.CTRL.GLOBAL_RESET"), 1)
 
     sleep(0.3)
-    writeReg(getNode("GEM_AMC.GEM_SYSTEM.CTRL.LINK_RESET"), 1)
+    writeReg(getNode("CSC_FED.GEM_SYSTEM.CTRL.LINK_RESET"), 1)
 
     print("DONE")
 
