2c55abab75638289c228d350b82cc22b76af629c
Andrew Peck <andrew.peck@cern.ch>
2021-07-02 22:43:15 -0400

    OH: Some asserts to check sorting...

--- gem/hdl/cluster_finding/hdl/find_clusters.vhd
+++ gem/hdl/cluster_finding/hdl/find_clusters.vhd
@@ -279,6 +279,32 @@ begin
 
   begin
 
+    checker : for I in 0 to NUM_FOUND_CLUSTERS generate
+      -- just some simple sanity checking for the simulator
+
+      -- make sure that every higher numbered cluster is worse or equal quality
+      worseloop : for J in I+1 to NUM_FOUND_CLUSTERS-1 generate
+
+        -- check vpf sorting
+        assert clusters(I).vpf >= clusters(J).vpf report
+          "vpf(" & integer'image(I) & ") = " & std_logic'image(clusters(I).vpf) & ";  " &
+          "vpf(" & integer'image(J) & ") = " & std_logic'image(clusters(J).vpf)
+          severity error;
+
+        -- for equal vpf, use the greater partition (lower eta)
+        prt_sort_check : if (clusters(I).vpf = '1' and (clusters(I).vpf = clusters(J).vpf)) generate
+          assert clusters(I).prt >= clusters(J).prt  report
+            "vpf(" & integer'image(I) & ") = " & std_logic'image(clusters(I).vpf) & " " &
+            "prt(" & integer'image(I) & ") = " & integer'image(to_integer(unsigned(clusters(I).prt))) & ";  " &
+            "prt(" & integer'image(J) & ") = " & integer'image(to_integer(unsigned(clusters(J).prt))) & " " &
+            "vpf(" & integer'image(J) & ") = " & std_logic'image(clusters(J).vpf)
+            severity error;
+        end generate;
+
+      end generate;
+
+    end generate;
+
     wrapup : for I in 0 to NUM_FOUND_CLUSTERS-1 generate
       constant hi : integer := size*(I+1)-1;
       constant lo : integer := size*(I);
