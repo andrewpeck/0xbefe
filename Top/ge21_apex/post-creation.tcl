5a37440554f4badafb23895ad887ebb46c5df5da
Andrew Peck <andrew.peck@cern.ch>
2021-08-18 14:47:33 -0400

    Set top file in .src files rather than tcl

--- Top/ge21_apex/post-creation.tcl
+++ Top/ge21_apex/post-creation.tcl
@@ -1,3 +1,2 @@
 set_property default_lib work [current_project]
-set_property top gem_apex [current_fileset]
 
--- Top/ge21_cvp13/post-creation.tcl
+++ Top/ge21_cvp13/post-creation.tcl
@@ -1,2 +1,1 @@
 set_property default_lib work [current_project]
-set_property top gem_cvp13 [current_fileset]
