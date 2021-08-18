c8eba700bb91ffed0e5f7ad5dcd5479c174c35a5
Andrew Peck <andrew.peck@cern.ch>
2021-08-18 14:51:24 -0400

    Add apex special defines to post-creation.tcl

--- Top/me0_apex/post-creation.tcl
+++ Top/me0_apex/post-creation.tcl
@@ -1,1 +1,6 @@
 set_property default_lib work [current_project]
+
+# required for APEX KU15P rev1
+set PATH_REPO "[file normalize [file dirname [info script]]]/../../"
+set_property verilog_define {C2C_R1_UEC3 C2C_3P125G} [current_fileset]
+set_property file_type {Verilog Header} [get_files $PATH_REPO/boards/apex/hdl/c2c/mgt/c2c_gth_example_wrapper_functions.v]
