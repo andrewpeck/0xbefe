04f485c2b3debc576f2afa3b231964974168e989
Andrew Peck <andrew.peck@cern.ch>
2021-08-19 15:19:26 -0400

    Try slink rocket magic

--- Top/csc_apex/post-creation.tcl
+++ Top/csc_apex/post-creation.tcl
@@ -1,4 +1,10 @@
 # required for APEX KU15P rev1
-set PATH_REPO "[file normalize [file dirname [info script]]]/../../"
 set_property verilog_define C2C_3P125G [current_fileset]
-set_property file_type {Verilog Header} [get_files $PATH_REPO/boards/apex/hdl/c2c/mgt/c2c_gth_example_wrapper_functions.v]
+
+# for some reason this is required in post-creation.tcl
+# otherwise vivado crashes with mysterious errors such as
+# error: [ip_flow 19-155] failed to convert to hdl value.
+catch {generate_target all [get_files slink_rocket_sender.xci]}
+
+set_property file_type {Verilog Header} \
+  [get_files c2c_gth_example_wrapper_functions.v]
