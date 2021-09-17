320f9996303c4ea90fa4d64fbaa9f04915b674b9
Andrew Peck <andrew.peck@cern.ch>
2021-09-17 13:33:40 -0400

    Remove apex post-creation catch
    (should be solved in new hog)

--- Top/csc_apex/post-creation.tcl
+++ Top/csc_apex/post-creation.tcl
@@ -1,7 +1,2 @@
 # required for APEX KU15P rev1
 set_property verilog_define C2C_3P125G [current_fileset]
-
-# for some reason this is required in post-creation.tcl
-# otherwise vivado crashes with mysterious errors such as
-# error: [ip_flow 19-155] failed to convert to hdl value.
-catch {generate_target all [get_files slink_rocket_sender.xci]}
--- Top/ge21_apex/post-creation.tcl
+++ Top/ge21_apex/post-creation.tcl
@@ -1,8 +1,2 @@
 # required for APEX KU15P rev1
-set PATH_REPO "[file normalize [file dirname [info script]]]/../../"
 set_property verilog_define {C2C_R1_UEC3 C2C_3P125G} [current_fileset]
-
-# for some reason this is required in post-creation.tcl
-# otherwise vivado crashes with mysterious errors such as
-# error: [ip_flow 19-155] failed to convert to hdl value.
-catch {generate_target all [get_files slink_rocket_sender.xci]}
