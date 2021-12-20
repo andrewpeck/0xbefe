9636ab383abd33c05ae937fe9ad0b347827ccedf
Andrew Peck <andrew.peck@cern.ch>
2021-12-20 12:40:27 -0500

    oh: add a l1a mask on the sbits

--- gem/hdl/oh_fe/pkg/registers.vhd
+++ gem/hdl/oh_fe/pkg/registers.vhd
@@ -10,7 +10,7 @@ package registers is
     -- Implements various control and monitoring functions of the Optohybrid
     --============================================================================
 
-    constant REG_CONTROL_NUM_REGS : integer := 42;
+    constant REG_CONTROL_NUM_REGS : integer := 43;
     constant REG_CONTROL_ADDRESS_MSB : integer := 5;
     constant REG_CONTROL_ADDRESS_LSB : integer := 0;
     constant REG_CONTROL_LOOPBACK_DATA_ADDR    : std_logic_vector(5 downto 0) := "00" & x"0";
@@ -118,169 +118,173 @@ package registers is
     constant REG_CONTROL_SBITS_CLUSTER_RATE_MSB    : integer := 31;
     constant REG_CONTROL_SBITS_CLUSTER_RATE_LSB     : integer := 0;
 
-    constant REG_CONTROL_HDMI_SBIT_SEL0_ADDR    : std_logic_vector(5 downto 0) := "01" & x"4";
+    constant REG_CONTROL_SBITS_CLUSTER_RATE_MASKED_ADDR    : std_logic_vector(5 downto 0) := "01" & x"4";
+    constant REG_CONTROL_SBITS_CLUSTER_RATE_MASKED_MSB    : integer := 31;
+    constant REG_CONTROL_SBITS_CLUSTER_RATE_MASKED_LSB     : integer := 0;
+
+    constant REG_CONTROL_HDMI_SBIT_SEL0_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
     constant REG_CONTROL_HDMI_SBIT_SEL0_MSB    : integer := 4;
     constant REG_CONTROL_HDMI_SBIT_SEL0_LSB     : integer := 0;
     constant REG_CONTROL_HDMI_SBIT_SEL0_DEFAULT : std_logic_vector(4 downto 0) := '0' & x"0";
 
-    constant REG_CONTROL_HDMI_SBIT_SEL1_ADDR    : std_logic_vector(5 downto 0) := "01" & x"4";
+    constant REG_CONTROL_HDMI_SBIT_SEL1_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
     constant REG_CONTROL_HDMI_SBIT_SEL1_MSB    : integer := 9;
     constant REG_CONTROL_HDMI_SBIT_SEL1_LSB     : integer := 5;
     constant REG_CONTROL_HDMI_SBIT_SEL1_DEFAULT : std_logic_vector(9 downto 5) := '0' & x"0";
 
-    constant REG_CONTROL_HDMI_SBIT_SEL2_ADDR    : std_logic_vector(5 downto 0) := "01" & x"4";
+    constant REG_CONTROL_HDMI_SBIT_SEL2_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
     constant REG_CONTROL_HDMI_SBIT_SEL2_MSB    : integer := 14;
     constant REG_CONTROL_HDMI_SBIT_SEL2_LSB     : integer := 10;
     constant REG_CONTROL_HDMI_SBIT_SEL2_DEFAULT : std_logic_vector(14 downto 10) := '0' & x"0";
 
-    constant REG_CONTROL_HDMI_SBIT_SEL3_ADDR    : std_logic_vector(5 downto 0) := "01" & x"4";
+    constant REG_CONTROL_HDMI_SBIT_SEL3_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
     constant REG_CONTROL_HDMI_SBIT_SEL3_MSB    : integer := 19;
     constant REG_CONTROL_HDMI_SBIT_SEL3_LSB     : integer := 15;
     constant REG_CONTROL_HDMI_SBIT_SEL3_DEFAULT : std_logic_vector(19 downto 15) := '0' & x"0";
 
-    constant REG_CONTROL_HDMI_SBIT_SEL4_ADDR    : std_logic_vector(5 downto 0) := "01" & x"4";
+    constant REG_CONTROL_HDMI_SBIT_SEL4_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
     constant REG_CONTROL_HDMI_SBIT_SEL4_MSB    : integer := 24;
     constant REG_CONTROL_HDMI_SBIT_SEL4_LSB     : integer := 20;
     constant REG_CONTROL_HDMI_SBIT_SEL4_DEFAULT : std_logic_vector(24 downto 20) := '0' & x"0";
 
-    constant REG_CONTROL_HDMI_SBIT_SEL5_ADDR    : std_logic_vector(5 downto 0) := "01" & x"4";
+    constant REG_CONTROL_HDMI_SBIT_SEL5_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
     constant REG_CONTROL_HDMI_SBIT_SEL5_MSB    : integer := 29;
     constant REG_CONTROL_HDMI_SBIT_SEL5_LSB     : integer := 25;
     constant REG_CONTROL_HDMI_SBIT_SEL5_DEFAULT : std_logic_vector(29 downto 25) := '0' & x"0";
 
-    constant REG_CONTROL_HDMI_SBIT_SEL6_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_SEL6_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_SEL6_MSB    : integer := 4;
     constant REG_CONTROL_HDMI_SBIT_SEL6_LSB     : integer := 0;
     constant REG_CONTROL_HDMI_SBIT_SEL6_DEFAULT : std_logic_vector(4 downto 0) := '0' & x"0";
 
-    constant REG_CONTROL_HDMI_SBIT_SEL7_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_SEL7_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_SEL7_MSB    : integer := 9;
     constant REG_CONTROL_HDMI_SBIT_SEL7_LSB     : integer := 5;
     constant REG_CONTROL_HDMI_SBIT_SEL7_DEFAULT : std_logic_vector(9 downto 5) := '0' & x"0";
 
-    constant REG_CONTROL_HDMI_SBIT_MODE0_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_MODE0_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_MODE0_MSB    : integer := 11;
     constant REG_CONTROL_HDMI_SBIT_MODE0_LSB     : integer := 10;
     constant REG_CONTROL_HDMI_SBIT_MODE0_DEFAULT : std_logic_vector(11 downto 10) := "00";
 
-    constant REG_CONTROL_HDMI_SBIT_MODE1_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_MODE1_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_MODE1_MSB    : integer := 13;
     constant REG_CONTROL_HDMI_SBIT_MODE1_LSB     : integer := 12;
     constant REG_CONTROL_HDMI_SBIT_MODE1_DEFAULT : std_logic_vector(13 downto 12) := "00";
 
-    constant REG_CONTROL_HDMI_SBIT_MODE2_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_MODE2_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_MODE2_MSB    : integer := 15;
     constant REG_CONTROL_HDMI_SBIT_MODE2_LSB     : integer := 14;
     constant REG_CONTROL_HDMI_SBIT_MODE2_DEFAULT : std_logic_vector(15 downto 14) := "00";
 
-    constant REG_CONTROL_HDMI_SBIT_MODE3_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_MODE3_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_MODE3_MSB    : integer := 17;
     constant REG_CONTROL_HDMI_SBIT_MODE3_LSB     : integer := 16;
     constant REG_CONTROL_HDMI_SBIT_MODE3_DEFAULT : std_logic_vector(17 downto 16) := "00";
 
-    constant REG_CONTROL_HDMI_SBIT_MODE4_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_MODE4_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_MODE4_MSB    : integer := 19;
     constant REG_CONTROL_HDMI_SBIT_MODE4_LSB     : integer := 18;
     constant REG_CONTROL_HDMI_SBIT_MODE4_DEFAULT : std_logic_vector(19 downto 18) := "00";
 
-    constant REG_CONTROL_HDMI_SBIT_MODE5_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_MODE5_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_MODE5_MSB    : integer := 21;
     constant REG_CONTROL_HDMI_SBIT_MODE5_LSB     : integer := 20;
     constant REG_CONTROL_HDMI_SBIT_MODE5_DEFAULT : std_logic_vector(21 downto 20) := "00";
 
-    constant REG_CONTROL_HDMI_SBIT_MODE6_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_MODE6_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_MODE6_MSB    : integer := 23;
     constant REG_CONTROL_HDMI_SBIT_MODE6_LSB     : integer := 22;
     constant REG_CONTROL_HDMI_SBIT_MODE6_DEFAULT : std_logic_vector(23 downto 22) := "00";
 
-    constant REG_CONTROL_HDMI_SBIT_MODE7_ADDR    : std_logic_vector(5 downto 0) := "01" & x"5";
+    constant REG_CONTROL_HDMI_SBIT_MODE7_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
     constant REG_CONTROL_HDMI_SBIT_MODE7_MSB    : integer := 25;
     constant REG_CONTROL_HDMI_SBIT_MODE7_LSB     : integer := 24;
     constant REG_CONTROL_HDMI_SBIT_MODE7_DEFAULT : std_logic_vector(25 downto 24) := "00";
 
-    constant REG_CONTROL_CNT_SNAP_PULSE_ADDR    : std_logic_vector(5 downto 0) := "01" & x"6";
+    constant REG_CONTROL_CNT_SNAP_PULSE_ADDR    : std_logic_vector(5 downto 0) := "01" & x"7";
     constant REG_CONTROL_CNT_SNAP_PULSE_BIT    : integer := 0;
 
-    constant REG_CONTROL_CNT_SNAP_DISABLE_ADDR    : std_logic_vector(5 downto 0) := "01" & x"7";
+    constant REG_CONTROL_CNT_SNAP_DISABLE_ADDR    : std_logic_vector(5 downto 0) := "01" & x"8";
     constant REG_CONTROL_CNT_SNAP_DISABLE_BIT    : integer := 0;
     constant REG_CONTROL_CNT_SNAP_DISABLE_DEFAULT : std_logic := '1';
 
-    constant REG_CONTROL_DNA_DNA_LSBS_ADDR    : std_logic_vector(5 downto 0) := "01" & x"9";
+    constant REG_CONTROL_DNA_DNA_LSBS_ADDR    : std_logic_vector(5 downto 0) := "01" & x"a";
     constant REG_CONTROL_DNA_DNA_LSBS_MSB    : integer := 31;
     constant REG_CONTROL_DNA_DNA_LSBS_LSB     : integer := 0;
 
-    constant REG_CONTROL_DNA_DNA_MSBS_ADDR    : std_logic_vector(5 downto 0) := "01" & x"a";
+    constant REG_CONTROL_DNA_DNA_MSBS_ADDR    : std_logic_vector(5 downto 0) := "01" & x"b";
     constant REG_CONTROL_DNA_DNA_MSBS_MSB    : integer := 24;
     constant REG_CONTROL_DNA_DNA_MSBS_LSB     : integer := 0;
 
-    constant REG_CONTROL_UPTIME_ADDR    : std_logic_vector(5 downto 0) := "01" & x"b";
+    constant REG_CONTROL_UPTIME_ADDR    : std_logic_vector(5 downto 0) := "01" & x"c";
     constant REG_CONTROL_UPTIME_MSB    : integer := 19;
     constant REG_CONTROL_UPTIME_LSB     : integer := 0;
 
-    constant REG_CONTROL_USR_ACCESS_ADDR    : std_logic_vector(5 downto 0) := "10" & x"1";
+    constant REG_CONTROL_USR_ACCESS_ADDR    : std_logic_vector(5 downto 0) := "10" & x"2";
     constant REG_CONTROL_USR_ACCESS_MSB    : integer := 31;
     constant REG_CONTROL_USR_ACCESS_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_GLOBAL_DATE_ADDR    : std_logic_vector(5 downto 0) := "10" & x"3";
+    constant REG_CONTROL_HOG_GLOBAL_DATE_ADDR    : std_logic_vector(5 downto 0) := "10" & x"4";
     constant REG_CONTROL_HOG_GLOBAL_DATE_MSB    : integer := 31;
     constant REG_CONTROL_HOG_GLOBAL_DATE_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_GLOBAL_TIME_ADDR    : std_logic_vector(5 downto 0) := "10" & x"4";
+    constant REG_CONTROL_HOG_GLOBAL_TIME_ADDR    : std_logic_vector(5 downto 0) := "10" & x"5";
     constant REG_CONTROL_HOG_GLOBAL_TIME_MSB    : integer := 31;
     constant REG_CONTROL_HOG_GLOBAL_TIME_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_GLOBAL_VER_ADDR    : std_logic_vector(5 downto 0) := "10" & x"5";
+    constant REG_CONTROL_HOG_GLOBAL_VER_ADDR    : std_logic_vector(5 downto 0) := "10" & x"6";
     constant REG_CONTROL_HOG_GLOBAL_VER_MSB    : integer := 31;
     constant REG_CONTROL_HOG_GLOBAL_VER_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_GLOBAL_SHA_ADDR    : std_logic_vector(5 downto 0) := "10" & x"6";
+    constant REG_CONTROL_HOG_GLOBAL_SHA_ADDR    : std_logic_vector(5 downto 0) := "10" & x"7";
     constant REG_CONTROL_HOG_GLOBAL_SHA_MSB    : integer := 31;
     constant REG_CONTROL_HOG_GLOBAL_SHA_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_TOP_SHA_ADDR    : std_logic_vector(5 downto 0) := "10" & x"7";
+    constant REG_CONTROL_HOG_TOP_SHA_ADDR    : std_logic_vector(5 downto 0) := "10" & x"8";
     constant REG_CONTROL_HOG_TOP_SHA_MSB    : integer := 31;
     constant REG_CONTROL_HOG_TOP_SHA_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_TOP_VER_ADDR    : std_logic_vector(5 downto 0) := "10" & x"8";
+    constant REG_CONTROL_HOG_TOP_VER_ADDR    : std_logic_vector(5 downto 0) := "10" & x"9";
     constant REG_CONTROL_HOG_TOP_VER_MSB    : integer := 31;
     constant REG_CONTROL_HOG_TOP_VER_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_HOG_SHA_ADDR    : std_logic_vector(5 downto 0) := "10" & x"9";
+    constant REG_CONTROL_HOG_HOG_SHA_ADDR    : std_logic_vector(5 downto 0) := "10" & x"a";
     constant REG_CONTROL_HOG_HOG_SHA_MSB    : integer := 31;
     constant REG_CONTROL_HOG_HOG_SHA_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_HOG_VER_ADDR    : std_logic_vector(5 downto 0) := "10" & x"a";
+    constant REG_CONTROL_HOG_HOG_VER_ADDR    : std_logic_vector(5 downto 0) := "10" & x"b";
     constant REG_CONTROL_HOG_HOG_VER_MSB    : integer := 31;
     constant REG_CONTROL_HOG_HOG_VER_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_OH_SHA_ADDR    : std_logic_vector(5 downto 0) := "10" & x"b";
+    constant REG_CONTROL_HOG_OH_SHA_ADDR    : std_logic_vector(5 downto 0) := "10" & x"c";
     constant REG_CONTROL_HOG_OH_SHA_MSB    : integer := 31;
     constant REG_CONTROL_HOG_OH_SHA_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_OH_VER_ADDR    : std_logic_vector(5 downto 0) := "10" & x"c";
+    constant REG_CONTROL_HOG_OH_VER_ADDR    : std_logic_vector(5 downto 0) := "10" & x"d";
     constant REG_CONTROL_HOG_OH_VER_MSB    : integer := 31;
     constant REG_CONTROL_HOG_OH_VER_LSB     : integer := 0;
 
-    constant REG_CONTROL_HOG_FLAVOUR_ADDR    : std_logic_vector(5 downto 0) := "10" & x"d";
+    constant REG_CONTROL_HOG_FLAVOUR_ADDR    : std_logic_vector(5 downto 0) := "10" & x"e";
     constant REG_CONTROL_HOG_FLAVOUR_MSB    : integer := 31;
     constant REG_CONTROL_HOG_FLAVOUR_LSB     : integer := 0;
 
-    constant REG_CONTROL_TMR_TTC_TMR_ERR_CNT_ADDR    : std_logic_vector(5 downto 0) := "11" & x"1";
+    constant REG_CONTROL_TMR_TTC_TMR_ERR_CNT_ADDR    : std_logic_vector(5 downto 0) := "11" & x"2";
     constant REG_CONTROL_TMR_TTC_TMR_ERR_CNT_MSB    : integer := 15;
     constant REG_CONTROL_TMR_TTC_TMR_ERR_CNT_LSB     : integer := 0;
 
-    constant REG_CONTROL_TMR_IPB_SWITCH_TMR_ERR_CNT_ADDR    : std_logic_vector(5 downto 0) := "11" & x"2";
+    constant REG_CONTROL_TMR_IPB_SWITCH_TMR_ERR_CNT_ADDR    : std_logic_vector(5 downto 0) := "11" & x"3";
     constant REG_CONTROL_TMR_IPB_SWITCH_TMR_ERR_CNT_MSB    : integer := 31;
     constant REG_CONTROL_TMR_IPB_SWITCH_TMR_ERR_CNT_LSB     : integer := 16;
 
-    constant REG_CONTROL_TMR_IPB_SLAVE_TMR_ERR_CNT_ADDR    : std_logic_vector(5 downto 0) := "11" & x"3";
+    constant REG_CONTROL_TMR_IPB_SLAVE_TMR_ERR_CNT_ADDR    : std_logic_vector(5 downto 0) := "11" & x"4";
     constant REG_CONTROL_TMR_IPB_SLAVE_TMR_ERR_CNT_MSB    : integer := 15;
     constant REG_CONTROL_TMR_IPB_SLAVE_TMR_ERR_CNT_LSB     : integer := 0;
 
-    constant REG_CONTROL_TMR_TMR_CNT_RESET_ADDR    : std_logic_vector(5 downto 0) := "11" & x"4";
+    constant REG_CONTROL_TMR_TMR_CNT_RESET_ADDR    : std_logic_vector(5 downto 0) := "11" & x"5";
     constant REG_CONTROL_TMR_TMR_CNT_RESET_BIT    : integer := 0;
 
-    constant REG_CONTROL_TMR_TMR_ERR_INJ_ADDR    : std_logic_vector(5 downto 0) := "11" & x"5";
+    constant REG_CONTROL_TMR_TMR_ERR_INJ_ADDR    : std_logic_vector(5 downto 0) := "11" & x"6";
     constant REG_CONTROL_TMR_TMR_ERR_INJ_BIT    : integer := 0;
 
 
@@ -346,7 +350,7 @@ package registers is
     -- Connects to the trigger control module
     --============================================================================
 
-    constant REG_TRIG_NUM_REGS : integer := 106;
+    constant REG_TRIG_NUM_REGS : integer := 107;
     constant REG_TRIG_ADDRESS_MSB : integer := 8;
     constant REG_TRIG_ADDRESS_LSB : integer := 0;
     constant REG_TRIG_CTRL_VFAT_MASK_ADDR    : std_logic_vector(8 downto 0) := '0' & x"00";
@@ -1340,6 +1344,16 @@ package registers is
     constant REG_TRIG_TRIGGER_PRBS_EN_ADDR    : std_logic_vector(8 downto 0) := '0' & x"f9";
     constant REG_TRIG_TRIGGER_PRBS_EN_BIT    : integer := 0;
     constant REG_TRIG_TRIGGER_PRBS_EN_DEFAULT : std_logic := '0';
+
+    constant REG_TRIG_L1A_MASK_L1A_MASK_DELAY_ADDR    : std_logic_vector(8 downto 0) := '0' & x"fa";
+    constant REG_TRIG_L1A_MASK_L1A_MASK_DELAY_MSB    : integer := 4;
+    constant REG_TRIG_L1A_MASK_L1A_MASK_DELAY_LSB     : integer := 0;
+    constant REG_TRIG_L1A_MASK_L1A_MASK_DELAY_DEFAULT : std_logic_vector(4 downto 0) := '0' & x"0";
+
+    constant REG_TRIG_L1A_MASK_L1A_MASK_WIDTH_ADDR    : std_logic_vector(8 downto 0) := '0' & x"fa";
+    constant REG_TRIG_L1A_MASK_L1A_MASK_WIDTH_MSB    : integer := 12;
+    constant REG_TRIG_L1A_MASK_L1A_MASK_WIDTH_LSB     : integer := 8;
+    constant REG_TRIG_L1A_MASK_L1A_MASK_WIDTH_DEFAULT : std_logic_vector(12 downto 8) := '0' & x"0";
 
     constant REG_TRIG_TMR_CLUSTER_TMR_ERR_CNT_ADDR    : std_logic_vector(8 downto 0) := '1' & x"00";
     constant REG_TRIG_TMR_CLUSTER_TMR_ERR_CNT_MSB    : integer := 15;
--- gem/hdl/oh_fe/trigger/sbits.vhd
+++ gem/hdl/oh_fe/trigger/sbits.vhd
@@ -34,6 +34,11 @@ entity sbits is
 
     reset_i : in std_logic;
 
+    ttc : in ttc_t;
+
+    l1a_mask_delay : in std_logic_vector(4 downto 0);
+    l1a_mask_width : in std_logic_vector(4 downto 0);
+
     reverse_partitions : in std_logic                     := '0';
     sbit_map_sel       : in std_logic_vector (1 downto 0) := (others => '0');
 
@@ -51,7 +56,6 @@ entity sbits is
 
     aligned_count_to_ready : in std_logic_vector (11 downto 0);
 
-
     trigger_deadtime_i : in std_logic_vector (3 downto 0);
 
     sbits_p : in std_logic_vector (NUM_VFATS*8-1 downto 0);
@@ -63,9 +67,10 @@ entity sbits is
 
     active_vfats_o : out std_logic_vector (NUM_VFATS-1 downto 0);
 
-    clusters_o      : out sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
-    cluster_count_o : out std_logic_vector (10 downto 0);
-    overflow_o      : out std_logic;
+    clusters_o               : out sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
+    cluster_count_masked_o   : out std_logic_vector (10 downto 0);
+    cluster_count_unmasked_o : out std_logic_vector (10 downto 0);
+    overflow_o               : out std_logic;
 
     sot_is_aligned_o      : out std_logic_vector (NUM_VFATS-1 downto 0) := (others => '0');
     sot_unstable_o        : out std_logic_vector (NUM_VFATS-1 downto 0) := (others => '0');
@@ -86,6 +91,11 @@ entity sbits is
 end sbits;
 
 architecture Behavioral of sbits is
+
+  signal l1a_pipeline : std_logic_vector (31 downto 0) := (others => '0');
+  signal l1a_delayed  : std_logic;
+  signal l1a_mask_cnt : unsigned (4 downto 0)          := (others => '0');
+  signal mask_l1a     : std_logic;
 
   signal inject_sbits   : std_logic_vector (NUM_VFATS-1 downto 0) := (others => '0');
   signal inject_sbits_r : std_logic_vector (NUM_VFATS-1 downto 0) := (others => '0');
@@ -289,31 +299,64 @@ begin
       hitmap_o  => hitmap_sbits_o
       );
 
+  --------------------------------------------------------------------------------
+  -- L1A Delay
+  --------------------------------------------------------------------------------
+
+  process (clocks.clk40) is
+  begin
+    if (rising_edge(clocks.clk40)) then
+      l1a_pipeline(0) <= ttc.l1a;
+      for I in 1 to l1a_pipeline'left loop
+        l1a_pipeline(I) <= l1a_pipeline(I-1);
+      end loop;
+    end if;
+    l1a_delayed <= l1a_pipeline(to_integer(unsigned(l1a_mask_delay)));
+  end process;
+
+  process (clocks.clk40) is
+  begin
+    if (rising_edge(clocks.clk40)) then
+      if (ttc.l1a = '1') then
+        l1a_mask_cnt <= unsigned(l1a_mask_width);
+      elsif (l1a_mask_cnt > 0) then
+        l1a_mask_cnt <= l1a_mask_cnt - 1;
+      end if;
+    end if;
+  end process;
+
+  mask_l1a <= '1' when l1a_mask_cnt > 0 else '0';
+
   --------------------------------------------------------------------------------------------------------------------
   -- Cluster Packer
   --------------------------------------------------------------------------------------------------------------------
-
 
   cluster_packer_tmr : if (true) generate  -- generate for local scoped signals
 
     type sbit_cluster_array_array_t is array(integer range<>)
       of sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
 
-    signal clusters_tmr : sbit_cluster_array_array_t (2 downto 0);
+    signal clusters_unmasked_tmr : sbit_cluster_array_array_t (2 downto 0);
+    signal clusters_masked_tmr   : sbit_cluster_array_array_t (2 downto 0);
 
-    signal clusters       : sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
-    signal clusters_rev   : sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
-    signal clusters_norev : sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
+    signal clusters_masked   : sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
+    signal clusters_unmasked : sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
+    signal clusters_rev      : sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
+    signal clusters_norev    : sbit_cluster_array_t (NUM_FOUND_CLUSTERS-1 downto 0);
 
-    signal cluster_count : t_std11_array (2 downto 0);
-    signal overflow      : std_logic_vector (2 downto 0);
+    signal cluster_count_masked   : t_std11_array (2 downto 0);
+    signal cluster_count_unmasked : t_std11_array (2 downto 0);
 
-    attribute DONT_TOUCH                  : string;
-    attribute DONT_TOUCH of clusters_tmr  : signal is "true";
-    attribute DONT_TOUCH of cluster_count : signal is "true";
-    attribute DONT_TOUCH of overflow      : signal is "true";
+    signal overflow : std_logic_vector (2 downto 0);
 
-    signal cluster_tmr_err : std_logic_vector (2+NUM_FOUND_CLUSTERS-1 downto 0);
+    attribute DONT_TOUCH                           : string;
+    attribute DONT_TOUCH of clusters_unmasked_tmr  : signal is "true";
+    attribute DONT_TOUCH of clusters_masked_tmr    : signal is "true";
+    attribute DONT_TOUCH of cluster_count_masked   : signal is "true";
+    attribute DONT_TOUCH of cluster_count_unmasked : signal is "true";
+    attribute DONT_TOUCH of overflow               : signal is "true";
+
+    signal cluster_tmr_err : std_logic_vector (3+NUM_FOUND_CLUSTERS-1 downto 0);
 
     signal tmr_err_inj : std_logic := '0';
 
@@ -339,11 +382,17 @@ begin
           clk_fast => clocks.clk160_0,
           reset    => reset_i,
 
+          mask_output_i => mask_l1a,
+
           sbits_i => vfat_sbits_injected,
 
-          cluster_count_o => cluster_count(I),
-          clusters_o      => clusters_tmr(I),
-          overflow_o      => overflow(I)
+          clusters_o      => clusters_unmasked_tmr(I),
+          cluster_count_o => cluster_count_unmasked(I),
+
+          clusters_masked_o      => clusters_masked_tmr(I),
+          cluster_count_masked_o => cluster_count_masked(I),
+
+          overflow_o => overflow(I)
           );
     end generate;
 
@@ -351,25 +400,34 @@ begin
     begin
 
       majority_err (overflow_o, cluster_tmr_err(0), tmr_err_inj xor overflow(0), overflow(1), overflow(2));
-      majority_err (cluster_count_o, cluster_tmr_err(1), cluster_count(0), cluster_count(1), cluster_count(2));
+
+      majority_err (cluster_count_masked_o, cluster_tmr_err(1), cluster_count_masked(0), cluster_count_masked(1), cluster_count_masked(2));
+      majority_err (cluster_count_unmasked_o, cluster_tmr_err(2), cluster_count_unmasked(0), cluster_count_unmasked(1), cluster_count_unmasked(2));
 
       cluster_assign_loop : for I in 0 to NUM_FOUND_CLUSTERS-1 generate
-        signal err : std_logic_vector (3 downto 0) := (others => '0');
+        signal err : std_logic_vector (7 downto 0) := (others => '0');
       begin
 
-        majority_err (clusters(I).adr, err(0), clusters_tmr(0)(I).adr, clusters_tmr(1)(I).adr, clusters_tmr(2)(I).adr);
-        majority_err (clusters(I).cnt, err(1), clusters_tmr(0)(I).cnt, clusters_tmr(1)(I).cnt, clusters_tmr(2)(I).cnt);
-        majority_err (clusters(I).prt, err(2), clusters_tmr(0)(I).prt, clusters_tmr(1)(I).prt, clusters_tmr(2)(I).prt);
-        majority_err (clusters(I).vpf, err(3), clusters_tmr(0)(I).vpf, clusters_tmr(1)(I).vpf, clusters_tmr(2)(I).vpf);
+        majority_err (clusters_unmasked(I).adr, err(0), clusters_unmasked_tmr(0)(I).adr, clusters_unmasked_tmr(1)(I).adr, clusters_unmasked_tmr(2)(I).adr);
+        majority_err (clusters_unmasked(I).cnt, err(1), clusters_unmasked_tmr(0)(I).cnt, clusters_unmasked_tmr(1)(I).cnt, clusters_unmasked_tmr(2)(I).cnt);
+        majority_err (clusters_unmasked(I).prt, err(2), clusters_unmasked_tmr(0)(I).prt, clusters_unmasked_tmr(1)(I).prt, clusters_unmasked_tmr(2)(I).prt);
+        majority_err (clusters_unmasked(I).vpf, err(3), clusters_unmasked_tmr(0)(I).vpf, clusters_unmasked_tmr(1)(I).vpf, clusters_unmasked_tmr(2)(I).vpf);
 
-        cluster_tmr_err(2+I) <= or_reduce(err);
+        majority_err (clusters_masked(I).adr, err(4), clusters_masked_tmr(0)(I).adr, clusters_masked_tmr(1)(I).adr, clusters_masked_tmr(2)(I).adr);
+        majority_err (clusters_masked(I).cnt, err(5), clusters_masked_tmr(0)(I).cnt, clusters_masked_tmr(1)(I).cnt, clusters_masked_tmr(2)(I).cnt);
+        majority_err (clusters_masked(I).prt, err(6), clusters_masked_tmr(0)(I).prt, clusters_masked_tmr(1)(I).prt, clusters_masked_tmr(2)(I).prt);
+        majority_err (clusters_masked(I).vpf, err(7), clusters_masked_tmr(0)(I).vpf, clusters_masked_tmr(1)(I).vpf, clusters_masked_tmr(2)(I).vpf);
+
+        cluster_tmr_err(3+I) <= or_reduce(err);
       end generate;
     end generate;
 
     notmr_gen : if (EN_TMR /= 1) generate
-      clusters        <= clusters_tmr(0);
-      overflow_o      <= overflow(0);
-      cluster_count_o <= cluster_count(0);
+      clusters_masked          <= clusters_unmasked_tmr(0);
+      clusters_unmasked        <= clusters_masked_tmr(0);
+      overflow_o               <= overflow(0);
+      cluster_count_masked_o   <= cluster_count_masked(0);
+      cluster_count_unmasked_o <= cluster_count_unmasked(0);
     end generate;
 
     reverse_gen : for I in clusters_o'range generate
@@ -378,21 +436,21 @@ begin
       -- Reversed
       --------------------------------------------------------------------------------
 
-      clusters_rev(I).vpf <= clusters(I).vpf;
-      clusters_rev(I).cnt <= clusters(I).cnt;
-      clusters_rev(I).prt <= clusters(I).prt;
+      clusters_rev(I).vpf <= clusters_masked(I).vpf;
+      clusters_rev(I).cnt <= clusters_masked(I).cnt;
+      clusters_rev(I).prt <= clusters_masked(I).prt;
       --new_address = 384 - (address + size)  (GE21)
       --new_address = 191 - (address + size)  (GE11)
       clusters_rev(I).adr <=
         std_logic_vector(to_unsigned(MXSBITS*PARTITION_SIZE-1 -
-                                     (to_integer(unsigned(clusters(I).adr)) +
-                                      to_integer(unsigned(clusters(I).cnt))), clusters_rev(I).adr'length));
+                                     (to_integer(unsigned(clusters_masked(I).adr)) +
+                                      to_integer(unsigned(clusters_masked(I).cnt))), clusters_rev(I).adr'length));
 
       --------------------------------------------------------------------------------
       -- Non-reversed
       --------------------------------------------------------------------------------
 
-      clusters_norev(I) <= clusters(I);
+      clusters_norev(I) <= clusters_masked(I);
 
     end generate;
 
