f410c2b625eaeb5ed99bd66a3b55c67700d94812
Andrew Peck <andrew.peck@cern.ch>
2022-01-26 08:21:37 -0500

    oh: replace ip with xpm
    was failing in ci, thanks to different vivado versions
    
    better to get rid of ips anyway..

--- gem/hdl/oh_fe/mgt/legacy_link/gem_data_out.v
+++ gem/hdl/oh_fe/mgt/legacy_link/gem_data_out.v
@@ -354,18 +354,60 @@ module   gem_data_out
 
          assign overflow[3:1] = {3{overflow[0]}};
 
-         cluster_data_cdc_inst
-           cluster_data_cdc
-             (
-              .wr_clk        (clock_40),
-              .rd_clk        (txoutclk),
-              .din           ({gem_data[0],     bc0_i, resync_i, bxn_counter_i[1:0],  overflow_i}),
-              .dout          ({gem_data_sync[0],bc0,   resync,   bxn_counter_lsbs,    overflow[0]}),
-              .full          (),
-              .empty         (),
-              .wr_en         (ready),
-              .rd_en         (tx_frame[ilink]==0)
-              );
+         xpm_fifo_async
+           #(
+             .FIFO_MEMORY_TYPE    ("auto"),               // auto, block, distributed, ultra
+             .ECC_MODE            ("no_ecc"),             // no_ecc, en_ecc
+             .RELATED_CLOCKS      (1),                    // DECIMAL
+             .FIFO_WRITE_DEPTH    (16),                   // DECIMAL
+             .WRITE_DATA_WIDTH    (117),                  // DECIMAL
+             .WR_DATA_COUNT_WIDTH (5),                    // DECIMAL
+             .PROG_FULL_THRESH    (5),                    // DECIMAL
+             .FULL_RESET_VALUE    (0),                    // DECIMAL
+             .READ_MODE           ("std"),                // std or fwft
+             .FIFO_READ_LATENCY   (1),                    // DECIMAL
+             .READ_DATA_WIDTH     (117),                  // DECIMAL
+             .RD_DATA_COUNT_WIDTH (5),                    // DECIMAL
+             .PROG_EMPTY_THRESH   (5),                    // DECIMAL
+             .DOUT_RESET_VALUE    ("0"),                  // String
+             .CDC_SYNC_STAGES     (2),                    // DECIMAL
+             .WAKEUP_TIME         (0)                     // 0 = disable sleep, 2 = use sleep pin
+             // VALID(12) = 1 ; AEMPTY(11) = 0; RD_DATA_CNT(10) = 0; PROG_EMPTY(9) = 1;
+             // UNDERFLOW(8) = 1; -- WR_ACK(4) = 0; AFULL(3) = 0; WR_DATA_CNT(2) = 1; PROG_FULL(1) = 1; OVERFLOW(0) = 1
+             //.USE_ADV_FEATURES    ("0000")
+             )
+         xpm_fifo_async_inst
+           (
+            .din           ({gem_data[0],     bc0_i, resync_i, bxn_counter_i[1:0],  overflow_i}),
+            .dout          ({gem_data_sync[0],bc0,   resync,   bxn_counter_lsbs,    overflow[0]}),
+            .wr_clk        (clock_40), // write at 40
+            .rd_clk        (txoutclk), // read at  160/200
+            .wr_en         (ready),
+            .rd_en         (tx_frame[ilink]==0),
+            .rst           (reset_i),    // 1-bit input: Reset: Must be synchronous to wr_clk. Must be applied only when wr_clk is stable and free-running.
+
+            // .full          (),
+            // .empty         (),
+            // .data_valid    (),       // 1-bit output: Read Data Valid: When asserted, this signal indicates that valid data is available on the output bus (dout).
+            // .empty         (),       // 1-bit output: Empty Flag: When asserted, this signal indicates that the FIFO is empty. Read requests are ignored when the FIFO is empty, initiating a read while empty is not destructive to the FIFO.
+            // .full          (),       // 1-bit output: Full Flag: When asserted, this signal indicates that the FIFO is full. Write requests are ignored when the FIFO is full, initiating a write when the FIFO is full is not destructive to the contents of the FIFO.
+            // .almost_empty  (),       // 1-bit output: Almost Empty : When asserted, this signal indicates that only one more read can be performed before the FIFO goes to empty.
+            // .almost_full   (),       // 1-bit output: Almost Full: When asserted, this signal indicates that only one more write can be performed before the FIFO is full.
+            // .dbiterr       (),       // 1-bit output: Double Bit Error: Indicates that the ECC decoder detected a double-bit error and data in the FIFO core is corrupted.
+            // .overflow      (),       // 1-bit output: Overflow: This signal indicates that a write request (wren) during the prior clock cycle was rejected, because the FIFO is full. Overflowing the FIFO is not destructive to the contents of the FIFO.
+            // .prog_empty    (),       // 1-bit output: Programmable Empty: This signal is asserted when the number of words in the FIFO is less than or equal to the programmable empty threshold value. It is de-asserted when the number of words in the FIFO exceeds the programmable empty threshold value.
+            // .prog_full     (),       // 1-bit output: Programmable Full: This signal is asserted when the number of words in the FIFO is greater than or equal to the programmable full threshold value. It is de-asserted when the number of words in the FIFO is less than the programmable full threshold value.
+            // .rd_data_count (),       // RD_DATA_COUNT_WIDTH-bit output: Read Data Count: This bus indicates the number of words read from the FIFO.
+            // .rd_rst_busy   (),       // 1-bit output: Read Reset Busy: Active-High indicator that the FIFO read domain is currently in a reset state.
+            // .sbiterr       (),       // 1-bit output: Single Bit Error: Indicates that the ECC decoder detected and fixed a single-bit error.
+            // .underflow     (),       // 1-bit output: Underflow: Indicates that the read request (rd_en) during the previous clock cycle was rejected because the FIFO is empty. Under flowing the FIFO is not destructive to the FIFO.
+            // .wr_ack        (),       // 1-bit output: Write Acknowledge: This signal indicates that a write request (wr_en) during the prior clock cycle is succeeded.
+            // .wr_data_count (),       // WR_DATA_COUNT_WIDTH-bit output: Write Data Count: This bus indicates the number of words written into the FIFO.
+            // .wr_rst_busy   (),       // 1-bit output: Write Reset Busy: Active-High indicator that the FIFO write domain is currently in a reset state.
+            .injectdbiterr (0),      // 1-bit input: Double Bit Error Injection: Injects a double bit error if the ECC feature is used on block RAMs or UltraRAM macros.
+            .injectsbiterr (0),      // 1-bit input: Single Bit Error Injection: Injects a single bit error if the ECC feature is used on block RAMs or UltraRAM macros.
+            .sleep         (0)       // 1-bit input: Dynamic power saving- If sleep is High, the memory/fifo block is in power saving mode.
+            );
 
          a7_gtp_wrapper_inst
            a7_gtp_wrapper
