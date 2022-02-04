11d53957a6a53222d985a90609dd10747ca306e4
Evaldas Juska <evaldas.juska@cern.ch>
2022-02-04 15:11:45 +0200

    TTC related fixes, also added a script to configure CCB

--- scripts/csc/ttc_tx.py
+++ scripts/csc/ttc_tx.py
@@ -1,42 +1,43 @@
 import sys
+from common.rw_reg import *
 from common.utils import *
 
-def hamming_8_13(data):
-    dbits = []
-    for i in range(8):
-        bit = (data >> i) & 1
-        dbits.append(bit)
+def ttc_ccb_init():
 
-    hbits = [0, 0, 0, 0, 0]
+    print("Initializing CCB")
 
-    hbits[0] = dbits[0] ^ dbits[1] ^ dbits[2] ^ dbits[3]
-    hbits[1] = dbits[0] ^ dbits[4] ^ dbits[5] ^ dbits[6]
-    hbits[2] = dbits[1] ^ dbits[2] ^ dbits[4] ^ dbits[5] ^ dbits[7]
-    hbits[3] = dbits[1] ^ dbits[3] ^ dbits[4] ^ dbits[6] ^ dbits[7]
-    hbits[4] = hbits[0] ^ hbits[1] ^ hbits[2] ^ hbits[3] ^ dbits[0] ^ dbits[1] ^ dbits[2] ^ dbits[3] ^ dbits[4] ^ dbits[5] ^ dbits[6] ^ dbits[7]
+    # write_reg("BEFE.TTC_TX.CTRL.RESET", 1)
 
-    hamming = (hbits[4] << 4) | (hbits[3] << 3) | (hbits[2] << 2) | (hbits[1] << 1) | (hbits[0] << 0)
+    # disable L1As and B commands
+    write_reg("BEFE.TTC_TX.CTRL.TTC_RX.L1A_EN", 0)
+    write_reg("BEFE.TTC_TX.CTRL.TTC_RX.BCMD_EN", 0)
 
-    return hamming
+    # configure the TTCrx
+    # 1) TTCrx addr = 0, E = 0 (internal), subaddr = 0 (fine delay reg 1), data = 0
+    # 2) TTCrx addr = 0, E = 0 (internal), subaddr = 1 (fine delay reg 2), data = 0
+    # 3) TTCrx addr = 0, E = 0 (internal), subaddr = 1 (coarse delay reg), data = 0
+    # 4) TTCrx addr = 0, E = 0 (internal), subaddr = 3 (control reg), data = 0xB1  (as far as I can see from TTCrx manual, this does the following: enable bunch counter, enable double bit error init, enable parallel addr/data bus, enable non-deskewed 40MHz output, and disables event counter, single bit err init, serial addr/data bus)
+    # 5) TTCrx addr = 0, E = 1 (external), subaddr = 0, data = 0 (as far as I understand, this will assert the data strobe DoutStr, and set the Dout[7:0] = 0, SubAddr[7:0] = 0, DQ[3:0] = 0)
+    write_reg("BEFE.TTC_TX.CTRL.SEND_MANUAL_ADDR_CMD", 0x00010000)
+    write_reg("BEFE.TTC_TX.CTRL.SEND_MANUAL_ADDR_CMD", 0x00010100)
+    write_reg("BEFE.TTC_TX.CTRL.SEND_MANUAL_ADDR_CMD", 0x00010200)
+    write_reg("BEFE.TTC_TX.CTRL.SEND_MANUAL_ADDR_CMD", 0x000103b1)
+    write_reg("BEFE.TTC_TX.CTRL.SEND_MANUAL_ADDR_CMD", 0x00030000)
 
-def main(cmd):
-    print("Command: %s" % hex_padded(cmd, 1))
-    hamming = hamming_8_13(cmd)
-
-    # frame = (0 << 15)      | '''START''' \
-    #         (0 << 14)      | '''FMT (Broadcast)''' \
-    #         (cmd << 6)     | '''data''' \
-    #         (hamming << 1) | '''hamming code''' \
-    #         1                '''STOP'''
-
-    frame = (0 << 15) | (0 << 14) | (cmd << 6) | (hamming << 1) | 1
-
-    print("Encoded B channel data: %s" % hex_padded(frame, 2))
+    # enable L1As and B commands
+    write_reg("BEFE.TTC_TX.CTRL.TTC_RX.L1A_EN", 1)
+    write_reg("BEFE.TTC_TX.CTRL.TTC_RX.BCMD_EN", 1)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("Usage: ttc_tx.py <broadcast_command_data>")
+        print("Usage: ttc_tx.py <command>")
+        print("Commands:")
+        print("    init: configures the CCB")
         exit(0)
 
-    cmd = parse_int(sys.argv[1])
-    main(cmd)
+    cmd = sys.argv[1]
+
+    parse_xml()
+
+    if cmd == "init":
+        ttc_ccb_init()
