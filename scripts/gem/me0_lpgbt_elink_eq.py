463bda7404d5ebbf3d0155cba7b0e4191b0eb7e1
Abhisek Datta <abhisek.datta@cern.ch>
2022-02-14 11:40:17 -0800

    add elink eq script

--- scripts/gem/me0_lpgbt_elink_eq.py
+++ scripts/gem/me0_lpgbt_elink_eq.py
@@ -13,7 +13,7 @@ def get_elink_group_channel(elink):
 
 def set_eq_setting(elink, eq0, eq1):
     group, channel = get_elink_group_channel(elink)
-    writeReg(getNode("LPGBT.RWF.EPORTRX.EPRX%d%dEQ0"%(group, channel), eq0, 0)
+    writeReg(getNode("LPGBT.RWF.EPORTRX.EPRX%d%dEQ0"%(group, channel)), eq0, 0)
     writeReg(getNode("LPGBT.RWF.EPORTRX.EPRX_CHN_CONTROL.EPRX%dEQ1"%elink), eq1, 0)
 
 def main(system, oh_select, vfat_list, sbit_elink_list, daq, eq):
