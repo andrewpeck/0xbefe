073eb2c58dfaa129e0471564409c17b07672be4d
Abhisek Datta <abhisek.datta@cern.ch>
2022-02-20 18:43:20 -0800

    update sbit scripts

--- scripts/gem/me0_vfat_sbit_phase_scan.py
+++ scripts/gem/me0_vfat_sbit_phase_scan.py
@@ -488,7 +488,7 @@ if __name__ == "__main__":
 
     # Running Phase Scan
     try:
-        vfat_sbit(args.gem, args.system, int(args.ohid), vfat_list, int(nl1a), args.calpulse_only, int(l1a_bxgap), set_cal_mode, cal_dac, bestphase_list)
+        vfat_sbit(args.gem, args.system, int(args.ohid), vfat_list, int(args.nl1a), args.calpulse_only, int(args.l1a_bxgap), set_cal_mode, cal_dac, bestphase_list)
     except KeyboardInterrupt:
         print (Colors.RED + "Keyboard Interrupt encountered" + Colors.ENDC)
         rw_terminate()
