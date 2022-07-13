144c01948aea9020ffb6d367f089f6d5e57669f0
Abhisek Datta <abhisek.datta@cern.ch>
2022-07-12 21:02:33 -0700

    daq test update

--- scripts/gem/vfat_daq_test.py
+++ scripts/gem/vfat_daq_test.py
@@ -153,14 +153,13 @@ def vfat_bert(gem, system, oh_select, vfat_list, set_cal_mode, cal_dac, nl1a, ru
                     real_calpulse_counter = nl1a_reg_cycles*(2**32) + calpulse_counter
                 else:
                     real_calpulse_counter = calpulse_counter
-                daq_event_count_temp = read_backend_reg(daq_event_count_node[vfat])
-                #daq_event_count_temp = real_l1a_counter # since DAQ_EVENT_CNT is a 16-bit rolling counter  
-                print ("Time passed: %.2f minutes, L1A counter = %.2e,  Calpulse counter = %.2e, DAQ Events = %.2e, L1A counter - DAQ Events = %d" % ((time()-t0)/60.0, real_l1a_counter, real_calpulse_counter, daq_event_count_temp, real_l1a_counter%(2**16) - daq_event_count_temp))
-                file_out.write("Time passed: %.2f minutes, L1A counter = %.2e,  Calpulse counter = %.2e, DAQ Events = %.2e, L1A counter - DAQ Events = %d\n" % ((time()-t0)/60.0, real_l1a_counter, real_calpulse_counter, daq_event_count_temp, real_l1a_counter%(2**16) - daq_event_count_temp))
+                print ("Time passed: %.2f minutes, L1A counter = %.2e,  Calpulse counter = %.2e" % ((time()-t0)/60.0, real_l1a_counter, real_calpulse_counter))
+                file_out.write("Time passed: %.2f minutes, L1A counter = %.2e,  Calpulse counter = %.2e\n" % ((time()-t0)/60.0, real_l1a_counter, real_calpulse_counter))
                 vfat_results_string = ""
                 for vfat in vfat_list:
+                    daq_event_count_temp = read_backend_reg(daq_event_count_node[vfat])
                     daq_error_count_temp = read_backend_reg(daq_crc_error_node[vfat])
-                    vfat_results_string += "VFAT %02d DAQ Errors: %d, "%(vfat, daq_error_count_temp)
+                    vfat_results_string += "VFAT %02d: DAQ Event Counter = %d, L1A Counter % 16 - DAQ Event Counter = %d, DAQ Errors = %d\n"%(vfat, daq_event_count_temp, real_l1a_counter%16 - daq_event_count_temp, daq_error_count_temp)
                 print (vfat_results_string + "\n")
                 file_out.write(vfat_results_string + "\n\n")
                 time_prev = time()
@@ -179,14 +178,13 @@ def vfat_bert(gem, system, oh_select, vfat_list, set_cal_mode, cal_dac, nl1a, ru
                     real_calpulse_counter = nl1a_reg_cycles*(2**32) + calpulse_counter
                 else:
                     real_calpulse_counter = calpulse_counter
-                daq_event_count_temp = read_backend_reg(daq_event_count_node[vfat])
-                #daq_event_count_temp = real_l1a_counter # since DAQ_EVENT_CNT is a 16-bit rolling counter  
-                print ("Time passed: %.2f minutes, L1A counter = %.2e,  Calpulse counter = %.2e, DAQ Events = %.2e, L1A counter - DAQ Events = %d" % ((time()-t0)/60.0, real_l1a_counter, real_calpulse_counter, daq_event_count_temp, real_l1a_counter%(2**16) - daq_event_count_temp))
-                file_out.write("Time passed: %.2f minutes, L1A counter = %.2e,  Calpulse counter = %.2e, DAQ Events = %.2e, L1A counter - DAQ Events = %d\n" % ((time()-t0)/60.0, real_l1a_counter, real_calpulse_counter, daq_event_count_temp, real_l1a_counter%(2**16) - daq_event_count_temp))
+                print ("Time passed: %.2f minutes, L1A counter = %.2e,  Calpulse counter = %.2e" % ((time()-t0)/60.0, real_l1a_counter, real_calpulse_counter))
+                file_out.write("Time passed: %.2f minutes, L1A counter = %.2e,  Calpulse counter = %.2e\n" % ((time()-t0)/60.0, real_l1a_counter, real_calpulse_counter))
                 vfat_results_string = ""
                 for vfat in vfat_list:
+                    daq_event_count_temp = read_backend_reg(daq_event_count_node[vfat])
                     daq_error_count_temp = read_backend_reg(daq_crc_error_node[vfat])
-                    vfat_results_string += "VFAT %02d DAQ Errors: %d, "%(vfat, daq_error_count_temp)
+                    vfat_results_string += "VFAT %02d: DAQ Event Counter = %d, L1A Counter % 16 - DAQ Event Counter = %d, DAQ Errors = %d\n"%(vfat, daq_event_count_temp, real_l1a_counter%16 - daq_event_count_temp, daq_error_count_temp)
                 print (vfat_results_string + "\n")
                 file_out.write(vfat_results_string + "\n\n")
                 time_prev = time()
