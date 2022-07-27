ae2336de22da157a71fc0c9885a9f6e20ad1a628
Abhisek Datta <abhisek.datta@cern.ch>
2022-07-27 16:13:22 -0700

    minor update

--- scripts/gem/me0_lpgbt/qsfp_testing/qsfp_voa_bert.py
+++ scripts/gem/me0_lpgbt/qsfp_testing/qsfp_voa_bert.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
                 look_for_keys=False)
 
     # Set Attenuation to 0
-    #ssh_command = "cd devel_scripts_update_0xbefe/0xbefe/scripts; source env.sh me0 cvp13 0; cd gem; python3 me0_lpgbt/qsfp_testing/voa_control.py -r -a 0"
-    ssh_command = "python3 Documents/voa_control.py -r -a 0"
+    #ssh_command = "cd devel_scripts_update_0xbefe/0xbefe/scripts; source env.sh me0 cvp13 0; cd gem; python3 me0_lpgbt/qsfp_testing/voa_control.py -a 0"    
+    ssh_command = "python3 Documents/voa_control.py -a 0"
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(ssh_command)
     output = ssh_stdout.readlines()
     print(output)
