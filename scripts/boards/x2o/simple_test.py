c0bdb37b7670333705a1ebdd5553c0c604381218
jewillia <jessica314@LAPTOP-ESO0S3EG.localdomain>
2022-10-25 09:50:31 -0500

    clean output, add P and I

--- scripts/boards/x2o/simple_test.py
+++ scripts/boards/x2o/simple_test.py
@@ -7,26 +7,32 @@ from datetime import datetime
 
 # Constants (to be moved to config file later)
 STABILIZE_SENSITIVITY = 0.01
-
+HEADERS = ["12V0_V","3V3_STANDBY_V","3V3_SI5395J_V","1V8_SI5395J_XO2_V","2V5_OSC_NE_V","1V8_MGTVCCAUX_VUP_N_V","2V5_OSC_NW_V","2V5_OSC_K7_V","1V2_MGTAVTT_K7_V","1V0_MGTAVCC_K7_V","0V675_DDRVTT_V","1V35_DDR_V","1V8_VCCAUX_K7_V","2V5_OSC_SE_V","1V8_MGTVCCAUX_VUP_S_V","2V5_OSC_SW_V",
+            "0V85_VCCINT_VUP_T0","0V85_VCCINT_VUP_T1","0V85_VCCINT_VUP_T2","0V85_VCCINT_VUP_T3","0V85_VCCINT_VUP_T4","0V85_VCCINT_VUP_T5",'1V2_MGTAVTT_VUP_S_TL','1V2_MGTAVTT_VUP_S_TR','1V2_MGTAVTT_VUP_S_TL','1V2_MGTAVTT_VUP_S_TR','KINTEX7_TL','KINTEX7_TR','0V9_MGTAVCC_VUP_S_TL','0V9_MGTAVCC_VUP_S_TR','0V9_MGTAVCC_VUP_S_TL','0V9_MGTAVCC_VUP_S_TR','2V7_INTERMEDIATE_TL','2V7_INTERMEDIATE_TR','1V2_MGTAVTT_VUP_N_TL','1V2_MGTAVTT_VUP_N_TR','VIRTEXUPLUS_TL','VIRTEXUPLUS_TR','0V9_MGTAVCC_VUP_S_TL','0V9_MGTAVCC_VUP_S_TR','TOTAL_ERRORS']
+V_EXPECT={'12V0':12,'3V3_STANDBY':3.3,'3V3_SI5395J':3.3,'1V8_SI5395J_XO2':1.8,'2V5_OSC_NE':2.5,'1V8_MGTVCCAUX_VUP_N':1.8,'2V5_OSC_NW':2.5,'2V5_OSC_K7':2.5,'1V2_MGTAVTT_K7':1.2,'1V0_MGTAVCC_K7':1.0,'0V675_DDRVTT':0.675,'1V35_DDR':1.35,'1V8_VCCAUX_K7':1.8,'2V5_OSC_SE':2.5,'1V8_MGTVCCAUX_VUP_S':1.8,'2V5_OSC_SW':2.5,'0V9_MGTAVCC_VUP_N':0.9,'1V8_VCCAUX_VUP':1.8,'1V0_VCCINT_K7':1.0,'1V2_MGTAVTT_VUP_N':1.2,'2V7_INTERMEDIATE':2.7,'0V9_MGTAVCC_VUP_S':0.9,'1V2_MGTAVTT_VUP_S':1.2}
 # End of constants
 
 
 def printRed(st):
-    print("\033[91m"+st+"\033[0m",end="")
+    return "\033[91m"+st+"\033[0m"
 
 def printGreen(st):
-    print("\033[92m"+st+"\033[0m",end="")
+    return "\033[92m"+st+"\033[0m"
 
 m=manager(optical_add_on_ver=2)
 m.peripheral.autodetect_optics()
 now = datetime.now()
 c_time = now.strftime("%Y-%m-%d-%H-%M-%S")
 f=open("./data/phys_data/pd_"+c_time+".csv","w")
+o_str=""
+for i in HEADERS:
+    o_str+=i+","
+f.write(o_str+"\n")
 diff_flag = True
-p_data = m.peripheral.monitor()
+p_data = m.peripheral.monitor(verbose=False)
 
 while(diff_flag):
-    data = m.peripheral.monitor()
+    data = m.peripheral.monitor(verbose=False)
     diff_flag=False
 
 
@@ -43,12 +49,10 @@ while(diff_flag):
         print(v_names[i],end=": ")
         if(v>v_expected[i]*1.05 or v<v_expected[i]*0.95):
             err_cnt+=1
-            printRed(str(v))
-            print("")
+            print(printRed(str(v)))
         else:
-            printGreen(str(v))
-            print("")
-        o_str+=v_names[i]+","+str(v)+"\n"
+            print(printGreen(str(v)))
+        o_str+=str(v)+","
 
     t_names=['1V2_MGTAVTT_VUP_S','1V2_MGTAVTT_VUP_S','KINTEX7','0V9_MGTAVCC_VUP_S','0V9_MGTAVCC_VUP_S','2V7_INTERMEDIATE','1V2_MGTAVTT_VUP_N','1V2_MGTAVTT_VUP_N','VIRTEXUPLUS','0V9_MGTAVCC_VUP_S','0V9_MGTAVCC_VUP_S']
     print("Temperatures")
@@ -63,12 +67,10 @@ while(diff_flag):
             diff_flag=True
         if(t>45):
             err_cnt+=1
-            printRed(str(t))
+            print(printRed(str(t)))
         else:
-            printGreen(str(t))
+            print(printGreen(str(t)))
         o_str+=str(t)+","
-    o_str+="\n"
-    print("")
 
     #write temp for rest
     for i in range(len(t_names)):
@@ -81,25 +83,75 @@ while(diff_flag):
         print(t_names[i],end=", left: ")
         if(tl>45):
             err_cnt+=1
-            printRed(str(tl))
+            print(printRed(str(tl)),end="")
         else:
-            printGreen(str(tl))
+            print(printGreen(str(tl)),end="")
         print(", right: ",end="")
         if(tr>45):
             err_cnt+=1
-            printRed(str(tr))
+            print(printRed(str(tr)))
         else:
-            printGreen(str(tr))
-        print("")
-        o_str+=t_names[i]+","+str(tl)+","+str(tr)+"\n"
+            print(printGreen(str(tr)))
+        o_str+=str(tl)+","+str(tr)+","
+
+    st=""
+    st=st+"{0:20}".format("Device")+'\tV\t\t'+"I\t\t"+'P\t\t'+'T\n'
+    for i in range(len(list(data.keys()))-1,0,-1):
+        device = list(data.keys())[i]
+        if device in ['optics']:
+            continue
+        st =st+ "{0:20}".format(device)
+        if 'V' in data[device].keys():
+            o_str+=str(data[device]['V'])+","
+            if (data[device]['V']>V_EXPECT[device]*1.05 or data[device]['V']<V_EXPECT[device]*0.95):
+                err_cnt+=1
+                st+='\t'+printRed(str(round(data[device]['V'],3)))
+            else:
+                st+='\t'+printGreen(str(round(data[device]['V'],3)))
+        else:
+            st=st+'\t'+'         '
+
+        if 'I' in data[device].keys():
+            o_str+=str(data[device]['I'])+","
+            st=st+'\t\t'+'{:+3.2f} A'.format(data[device]['I'])
+        else:
+            st=st+'\t\t'+'          '
+
+        if 'P' in data[device].keys():
+            o_str+=str(data[device]['P'])+","
+            st=st+'\t\t'+'{:+3.2f} W'.format(data[device]['P'])
+        else:
+            st=st+'\t\t'+'          '
+
+        if 'T' in data[device].keys():
+            tstr=[]
+            for t in data[device]['T']:
+                o_str+=str(t)+","
+                if t>45:
+                    tstr+=printRed(str(t))+" C,"
+                else:
+                    tstr+=printGreen(str(t))+" C,"
+
+            st=st+'\t\t'+','.join(tstr)
+        st=st+'\n'
+
+    st=st+'--------------------------------------------------------------------------------------------------------------\n'
+    #OPTICS"
+    if 'optics' in self.devices.keys() and len(self.devices['optics'].keys())>0:
+        st=st+'----------------------------------------------OPTICS----------------------------------------------------------\n'
+        st =st+ " cage {0:45}\t ".format("Type")+"{0:8}  ".format("V")+"{0:7} ".format("T")+" {0:4} ".format("RX")+" {0:4} ".format("TX")+" {0:5} ".format("RXCDR")+" {0:5} ".format("TXCDR")+" {0:12} ".format("RX Optical Power")+"\n"
+        for cage,info in data['optics'].items():
+            st =st+"{0:3}: ".format(str(cage))+ "{0:45}\t ".format(info['type'])+" {:2.3f} V".format(info['V'])+" {:+2.1f} C ".format(info['T'])+" {0:4} ".format(hex(info['rx_enabled']))+" {0:4} ".format(hex(info['tx_enabled']))+" {0:5} ".format(hex(info['rx_cdr']))+" {0:5} ".format(hex(info['tx_cdr']))+"  "','.join(info['rx_power'])+'\n'
+        st=st+'--------------------------------------------------------------------------------------------------------------\n'
 
     #final outputs for read cycle
-    o_str+="Total Errors,"+str(err_cnt)+"\n"
+    o_str+=str(err_cnt)+"\n"
     if(err_cnt>0):
         printRed("Total Errors: "+str(err_cnt))
     else:
         printGreen("Total Errors: "+str(err_cnt))
     print("")
+    print(st)
     f.write(o_str)
     p_data = data
     time.sleep(2)
