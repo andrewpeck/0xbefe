bbe043caf9b3c7f064d2897597bff0f330b11e82
Andrew Peck <andrew.peck@cern.ch>
2022-12-05 12:44:24 -0500

    promote multi-driven nets to errors at synthesis

--- Top/update-project.tcl
+++ Top/update-project.tcl
@@ -3,3 +3,8 @@ set PRJ_PATH "[file normalize [file dirname [info script]]]"
 source $PRJ_PATH/../common.tcl
 
 update_project_makefile update_${DESIGN}
+
+# promote multi-driven nets to errors so they are caught
+# at synthesis with a useful message instead of at the
+# end of implementation
+set_msg_config -id {Synth 8-6859} -new_severity {ERROR}
