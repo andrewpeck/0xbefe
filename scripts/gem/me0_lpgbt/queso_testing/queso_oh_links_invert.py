c06f9f1dc9e435c1aeecc669043eff296b0e4e38
Abhisek Datta <abhisek.datta@cern.ch>
2023-01-30 13:11:38 -0800

    bring elink inversion back for QUESO

--- scripts/gem/me0_lpgbt/queso_testing/queso_oh_links_invert.py
+++ scripts/gem/me0_lpgbt/queso_testing/queso_oh_links_invert.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
 
     # Parsing arguments
     parser = argparse.ArgumentParser(description="Invert elinks in ME0 OH")
-    parser.add_argument("-s", "--system", action="store", dest="system", help="system = chc or backend or dryrun")
+    parser.add_argument("-s", "--system", action="store", dest="system", help="system = chc or queso or backend or dryrun")
     parser.add_argument("-q", "--gem", action="store", dest="gem", help="gem = ME0 only")
     parser.add_argument("-o", "--ohid", action="store", dest="ohid", help="ohid = OH number")
     parser.add_argument("-g", "--gbtid", action="store", dest="gbtid", help="gbtid = GBT number")
@@ -76,12 +76,14 @@ if __name__ == "__main__":
 
     if args.system == "chc":
         print ("Using Rpi CHeeseCake for configuration")
+    elif args.system == "queso":
+        print ("Using QUESO for configuration")
     elif args.system == "backend":
         print ("Using Backend for configuration")
     elif args.system == "dryrun":
         print ("Dry Run - not actually configuring lpGBT")
     else:
-        print (Colors.YELLOW + "Only valid options: chc, backend, dryrun" + Colors.ENDC)
+        print (Colors.YELLOW + "Only valid options: chc, queso, backend, dryrun" + Colors.ENDC)
         sys.exit()
 
     if args.gem != "ME0":
