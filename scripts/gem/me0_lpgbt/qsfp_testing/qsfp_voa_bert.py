1fc23f3e7a5fdaf901421c8229e25f935fe552f3
Abhisek Datta <abhisek.datta@cern.ch>
2023-03-10 17:55:23 -0800

    add qsfp testing script update

--- scripts/gem/me0_lpgbt/qsfp_testing/qsfp_voa_bert.py
+++ scripts/gem/me0_lpgbt/qsfp_testing/qsfp_voa_bert.py
@@ -36,8 +36,7 @@ if __name__ == "__main__":
                 look_for_keys=False)
 
     # Set Attenuation to 0
-    ssh_command = "cd devel_scripts_update_0xbefe/0xbefe/scripts; source env.sh me0 cvp13 0; cd gem; python3 me0_lpgbt/qsfp_testing/voa_control.py -r -a 0"    
-    #ssh_command = "python3 Documents/voa_control.py -r -a 0"
+    ssh_command = "cd Documents/0xbefe/scripts; source env.sh me0 cvp13 0; cd gem; python3 me0_lpgbt/qsfp_testing/voa_control.py -r -a 0"    
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(ssh_command)
     output = ssh_stdout.readlines()
     print(output)
@@ -62,7 +61,6 @@ if __name__ == "__main__":
 
         # Run ssh command for VOA
         ssh_command = "cd Documents/0xbefe/scripts; source env.sh me0 cvp13 0; cd gem; python3 me0_lpgbt/qsfp_testing/voa_control.py -a %0.1f"%i  
-        ssh_command = "python3 Documents/voa_control.py -a %0.1f"%i
         print (ssh_command)
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(ssh_command)
         output = ssh_stdout.readlines()
