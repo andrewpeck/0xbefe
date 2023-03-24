6a9b6182fbaa80952c2f49c02b512087b277f56d
Abhisek Datta <abhisek.datta@cern.ch>
2023-03-24 08:10:26 -0700

    fix merge conflicts

--- scripts/gem/init_frontend.py
+++ scripts/gem/init_frontend.py
@@ -241,7 +241,6 @@ def init_gem_frontend():
             # Read in me0 DAQ phase scan results
             bestphase_list = {}
             phase_scan_filename = get_config("CONFIG_ME0_VFAT_PHASE_SCAN")
-<<<<<<< HEAD
             if phase_scan_filename != "":
                 phase_scan_filename = phase_scan_filename.split("*")[0] + str(oh) + phase_scan_filename.split("*")[1]
                 file_in = open(phase_scan_filename)
@@ -252,22 +251,10 @@ def init_gem_frontend():
                     phase = int(line.split()[1],16)
                     bestphase_list[vfat] = phase
                 file_in.close()
-=======
-            phase_scan_filename = phase_scan_filename.split("*")[0] + str(oh) + phase_scan_filename.split("*")[1]
-            file_in = open(phase_scan_filename)
-            for line in file_in.readlines():
-                if "vfat" in line:
-                    continue
-                vfat = int(line.split()[0])
-                phase = int(line.split()[1],16)
-                bestphase_list[vfat] = phase
-            file_in.close()
->>>>>>> origin
 
             # Read in sbit phase scan result
             bestphase_list_sbit = {}
             sbit_phase_scan_filename = get_config("CONFIG_ME0_VFAT_SBIT_PHASE_SCAN")
-<<<<<<< HEAD
             if sbit_phase_scan_filename != "":
                 sbit_phase_scan_filename = sbit_phase_scan_filename.split("*")[0] + str(oh) + sbit_phase_scan_filename.split("*")[1]
                 file_in = open(sbit_phase_scan_filename)
@@ -286,34 +273,11 @@ def init_gem_frontend():
             for vfat in range(0, 24):
                 lpgbt, gbt_num, elink_num, gpio = ME0_VFAT_TO_GBT_ELINK_GPIO[vfat] # Get the lpGBT and elink info for this VFAT
                 sbit_elinks = ME0_VFAT_TO_SBIT_ELINK[vfat] # Get the sbit elink info for this VFAT
-=======
-            sbit_phase_scan_filename = sbit_phase_scan_filename.split("*")[0] + str(oh) + sbit_phase_scan_filename.split("*")[1]
-            file_in = open(sbit_phase_scan_filename)
-            for line in file_in.readlines():
-                if "vfat" in line:
-                    continue
-                vfat = int(line.split()[0])
-                elink = int(line.split()[1])
-                phase = int(line.split()[2],16)
-                if vfat not in bestphase_list_sbit:
-                    bestphase_list_sbit[vfat] = {}
-                bestphase_list_sbit[vfat][elink] = phase
-            file_in.close()
-
-            # Set the phases
-            for vfat in range(0, 24):
-                set_bestphase = bestphase_list[vfat]
-                lpgbt, gbt_num, elink_num, gpio = ME0_VFAT_TO_GBT_ELINK_GPIO[vfat] # Get the lpGBT and elink info for this VFAT
->>>>>>> origin
                 gbt_ver = gbt_ver_list[gbt_num]
 
                 gbt_ready = read_reg("BEFE.GEM.OH_LINKS.OH%d.GBT%d_READY" % (oh, gbt_num))
                 if not gbt_ready: # Only set phases if GBT for this VFAT is READY
                     continue
-<<<<<<< HEAD
-=======
-                print ("\nSetting DAQ and Sbit phases for VFAT# %02d"%vfat)
->>>>>>> origin
 
                 oh_ver = -9999
                 if gbt_ver == 0:
@@ -340,7 +304,6 @@ def init_gem_frontend():
                     elif oh_ver == 2:
                         config = getConfig("../resources/me0_sub_config_ohv2.txt")
 
-<<<<<<< HEAD
                 if vfat in bestphase_list and vfat in bestphase_list_sbit:
                     print ("\nSetting DAQ and Sbit phases for OH %d VFAT# %02d"%(oh,vfat))
                 elif vfat in bestphase_list and vfat not in bestphase_list_sbit:
@@ -369,25 +332,6 @@ def init_gem_frontend():
                         writeGbtRegAddrs(addr, value)
                         sleep(0.1)
                         #print ("    OH %d VFAT %02d: Sbit Elink phase set for ELINK %02d to: %s" % (oh, vfat, elink, hex(set_bestphase)))
-=======
-                # Write DAQ phase setting to the lpGBT register for this VFAT
-                value = (config[addr] & 0x0f) | (set_bestphase << 4)
-                selectGbt(oh, gbt_num)
-                writeGbtRegAddrs(addr, value)
-                sleep(0.01)
-                #print ("DAQ Elink phase set for VFAT#%02d to: %s" % (vfat, hex(set_bestphase)))
-                
-                sbit_elinks = ME0_VFAT_TO_SBIT_ELINK[vfat] # Get the sbit elink info for this VFAT
-                # Get the lpGBT register address and write the phase setting for the 8 Sbit Elinks for this VFAT
-                for elink in range(0,8):
-                    set_bestphase = bestphase_list_sbit[vfat][elink]
-                    
-                    addr = GBT_ELINK_SAMPLE_PHASE_BASE_REG + sbit_elinks[elink]
-                    value = (config[addr] & 0x0f) | (set_bestphase << 4)
-                    writeGbtRegAddrs(addr, value)
-                    sleep(0.1)
-                    #print ("VFAT %02d: Sbit Elink phase set for ELINK %02d to: %s" % (vfat, elink, hex(set_bestphase)))
->>>>>>> origin
 
 
     print("\nSetting VFAT HDLC addresses")
