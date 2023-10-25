6a9aa5505e530eeafea76f9653fc4232efa937e6
root <root@localhost.localdomain>
2023-10-25 13:38:36 -0700

    fix merge conflicts

--- scripts/gem/me0_lpgbt/oh_testing/oh_production_tests.py
+++ scripts/gem/me0_lpgbt/oh_testing/oh_production_tests.py
@@ -2431,11 +2431,7 @@ if __name__ == "__main__":
     logfile.write("Step 16: S-bit Noise Rate\n\n")
     time.sleep(1)
 
-<<<<<<< HEAD
     if test_type in ["prototype", "pre_production", "pre_series", "production", "long_production", "acceptance"]:
-=======
-    if test_type in ["prototype", "pre_production", "pre_series", "production", "long_production", "acceptance",'debug']:
->>>>>>> origin
         for oh_select,gbt_vfat_dict in oh_gbt_vfat_map.items():
             print (Colors.BLUE + "Running S-bit Noise Rate for OH %d all VFATs\n"%oh_select + Colors.ENDC)
             logfile.write("Running S-bit Noise Rate for OH %d all VFATs\n\n"%oh_select)
