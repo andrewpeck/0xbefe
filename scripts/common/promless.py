885b9b441b62a77f2b09227cca24c4c7c653afa1
Evaldas Juska <evaldas.juska@cern.ch>
2023-10-26 16:01:21 +0300

    update scripts to support both CFEB and ALCT PROMless

--- scripts/common/promless.py
+++ scripts/common/promless.py
@@ -9,14 +9,19 @@ def bytesToWord(bytes, idx):
     return (bytes[idx + 3] << 24) | (bytes[idx + 2] << 16) | (bytes[idx + 1] << 8) | (bytes[idx])
     # return (bytes[idx + 0] << 24) | (bytes[idx + 1] << 16) | (bytes[idx + 2] << 8) | (bytes[idx + 3])
 
-def promless_load(bitfile_name, verify=True):
+# if promless_type is supplied, it will be appended to the PROMLESS node name in the address table (it's only used in CSC) e.g. if promless_type=ALCT, the function will use BEFE.PROMLESS_ALCT....
+def promless_load(bitfile_name, verify=True, promless_type=None):
+
+    node_suffix = ""
+    if promless_type is not None and len(promless_type > 0):
+        node_suffix = "_" + promless_type
 
     fname = bitfile_name
     if not path.exists(fname):
         printRed("Could not find %s" % fname)
         return
 
-    write_reg(get_node("BEFE.PROMLESS.RESET_ADDR"), 1)
+    write_reg(get_node("BEFE.PROMLESS%s.RESET_ADDR" % node_suffix), 1)
 
     print("Opening firmware bitstream file %s" % fname)
     f = open(fname, "rb")
@@ -40,24 +45,24 @@ def promless_load(bitfile_name, verify=True):
     print("Firmware bitstream size: %d bytes (%d words):" % (len(bytes), numWords))
 
     print("Writing PROMless firmware...")
-    wDataAddr = get_node("BEFE.PROMLESS.WRITE_DATA").address
+    wDataAddr = get_node("BEFE.PROMLESS%s.WRITE_DATA" % node_suffix).address
     for i in range(numWords):
         bidx = i * 4
         word = bytesToWord(bytes, bidx)
         wReg(wDataAddr, word)
 
-    write_reg(get_node("BEFE.PROMLESS.FIRMWARE_SIZE"), len(bytes))
+    write_reg(get_node("BEFE.PROMLESS%s.FIRMWARE_SIZE" % node_suffix), len(bytes))
     fw_flavor = read_reg("BEFE.SYSTEM.RELEASE.FW_FLAVOR")
     if fw_flavor.to_string() == "GEM":
-        write_reg(get_node("BEFE.GEM.GEM_SYSTEM.PROMLESS.FIRMWARE_SIZE"), len(bytes))
+        write_reg(get_node("BEFE.GEM.GEM_SYSTEM.PROMLESS%s.FIRMWARE_SIZE" % node_suffix), len(bytes))
     elif fw_flavor.to_string() == "CSC_FED":
-        write_reg(get_node("BEFE.CSC_FED.CSC_SYSTEM.PROMLESS.FIRMWARE_SIZE"), len(bytes))
+        write_reg(get_node("BEFE.CSC_FED.CSC_SYSTEM.PROMLESS%s.FIRMWARE_SIZE" % node_suffix), len(bytes))
     else:
         printRed("Unknown firmware flavor (%s), firmware size is not set in the promless loader (normally BEFE.GEM.GEM_SYSTEM.PROMLESS.FIRMWARE_SIZE or BEFE.CSC_FED.CSC_SYSTEM.PROMLESS.FIRMWARE_SIZE)" % fw_flavor.to_string(False))
 
     if verify:
         print("Verifying PROMless firmware...")
-        rDataAddr = get_node("BEFE.PROMLESS.READ_DATA").address
+        rDataAddr = get_node("BEFE.PROMLESS%s.READ_DATA" % node_suffix).address
         for i in range(numWords):
             wordReadback = rReg(rDataAddr)
             bidx = i * 4
@@ -68,8 +73,15 @@ def promless_load(bitfile_name, verify=True):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("Usage: promless_load.py <frontend_bitfile>")
+        print("Usage: promless_load.py <frontend_bitfile> [promless_type]")
+        print("    promless_type: in csc it's used to select either cfeb or alct promless block")
         exit()
 
+    fname = sys.argv[1]
+
+    promless_type = None
+    if len(sys.argv) > 2:
+        promless_type = sys.argv[2]
+
     parse_xml()
-    promless_load(sys.argv[1])
+    promless_load(fname, promless_type=promless_type)
