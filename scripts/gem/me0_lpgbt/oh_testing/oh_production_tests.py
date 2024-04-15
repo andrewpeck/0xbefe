694103c47f7f659174cbd6b5a3de704a9dd7e4f1
Abhisek Datta <abhisek.datta@cern.ch>
2024-04-15 12:13:12 -0700

    adc calib update

--- scripts/gem/me0_lpgbt/oh_testing/oh_production_tests.py
+++ scripts/gem/me0_lpgbt/oh_testing/oh_production_tests.py
@@ -2161,7 +2161,7 @@ if __name__ == "__main__":
     logfile.write("Step 12: DAQ SCurve\n\n")
     time.sleep(0.1)
 
-    if test_type in ["prototype", "pre_production", "pre_series", "production", "acceptance", "debug"]:
+    if test_type in ["prototype", "pre_production", "pre_series", "production", "acceptance"]:
         for oh_select,gbt_vfat_dict in oh_gbt_vfat_map.items():
             print (Colors.BLUE + "Running DAQ SCurves for OH %d all VFATs\n"%oh_select + Colors.ENDC)
             logfile.write("Running DAQ SCurves for OH %d all VFATs\n\n"%oh_select)
