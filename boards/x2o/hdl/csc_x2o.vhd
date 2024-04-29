c0c2b733b1b66db1264d92b7f415b3c288b5cb83
dgotler <dgotler@cern.ch>
2024-04-29 14:59:19 -0700

    Fix poly_length typo

--- boards/x2o/hdl/csc_x2o.vhd
+++ boards/x2o/hdl/csc_x2o.vhd
@@ -868,7 +868,7 @@ begin
             generic map(
                 CHK_MODE    => false,
                 INV_PATTERN => true,
-                POLY_LENGHT => 31,
+                POLY_LENGTH => 31,
                 POLY_TAP    => 28,
                 NBITS       => 32
             )
@@ -902,7 +902,7 @@ begin
                 generic map(
                     CHK_MODE    => true,
                     INV_PATTERN => true,
-                    POLY_LENGHT => 31,
+                    POLY_LENGTH => 31,
                     POLY_TAP    => 28,
                     NBITS       => 32
                 )
--- common/hdl/lpgbt/lpgbt.vhd
+++ common/hdl/lpgbt/lpgbt.vhd
@@ -503,7 +503,7 @@ begin
                 generic map(
                     CHK_MODE    => true,
                     INV_PATTERN => true,
-                    POLY_LENGHT => 31,
+                    POLY_LENGTH => 31,
                     POLY_TAP    => 28,
                     NBITS       => 32
                 )
@@ -545,7 +545,7 @@ begin
         generic map(
             CHK_MODE    => false,
             INV_PATTERN => true,
-            POLY_LENGHT => 31,
+            POLY_LENGTH => 31,
             POLY_TAP    => 28,
             NBITS       => 32
         )
