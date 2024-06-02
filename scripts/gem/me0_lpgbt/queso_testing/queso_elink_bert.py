c8e43bd7b314f578ffbe09b66316fe1f4b9f3f35
Abhisek Datta <abhisek.datta@cern.ch>
2024-06-02 15:21:41 -0700

    fix elink nr

--- scripts/gem/me0_lpgbt/queso_testing/queso_elink_bert.py
+++ scripts/gem/me0_lpgbt/queso_testing/queso_elink_bert.py
@@ -252,13 +252,13 @@ def queso_bert(system, queso_dict, oh_gbt_vfat_map, runtime, ber_limit, cl, loop
                 for gbt in oh_gbt_vfat_map[oh_select]["GBT"]:
                     fec_uplink_errors[oh_select][gbt] = read_backend_reg(fec_uplink_error_nodes[oh_select][gbt])
                     if fec_uplink_errors[oh_select][gbt] != 0:
-                        err_str += "    OH %d VFAT %d GBT %d: %d uplink errors\n"%(oh_select,vfat, gbt, fec_uplink_errors[oh_select][gbt])
+                        err_str += "    OH %d GBT %d: %d uplink errors\n"%(oh_select, gbt, fec_uplink_errors[oh_select][gbt])
                         n_link_fec_errors += 1
                     if gbt%2==0:
                         rw_reg_lpgbt.select_ic_link(oh_select, gbt)
                         fec_downlink_errors[oh_select][gbt] = lpgbt_fec_error_counter(oh_ver[oh_select][gbt])
                         if fec_downlink_errors[oh_select][gbt] != 0:
-                            err_str += "    OH %d VFAT %d GBT %d: %d downlink errors\n"%(oh_select,vfat, gbt, fec_uplink_errors[oh_select][gbt])
+                            err_str += "    OH %d GBT %d: %d downlink errors\n"%(oh_select, gbt, fec_uplink_errors[oh_select][gbt])
                             n_link_fec_errors += 1
             err_str += "\n" + Colors.ENDC
             if n_link_fec_errors == 0:
