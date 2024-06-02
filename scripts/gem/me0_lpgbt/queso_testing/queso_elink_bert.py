69d5134e6831e694f1082e389499512e8816ac08
Abhisek Datta <abhisek.datta@cern.ch>
2024-06-02 13:41:45 -0700

    fix elink nr

--- scripts/gem/me0_lpgbt/queso_testing/queso_elink_bert.py
+++ scripts/gem/me0_lpgbt/queso_testing/queso_elink_bert.py
@@ -341,10 +341,10 @@ def queso_bert(system, queso_dict, oh_gbt_vfat_map, runtime, ber_limit, cl, loop
                 else:
                     err_str += Colors.RED
                 lpgbt = ME0_VFAT_TO_GBT_ELINK_GPIO[vfat][1]
-                if elink == 0:
+                if elink == 8:
                     elink_nr = ME0_VFAT_TO_GBT_ELINK_GPIO[vfat][2]
                 else:
-                    elink_nr = ME0_VFAT_TO_SBIT_ELINK[vfat][elink-1]
+                    elink_nr = ME0_VFAT_TO_SBIT_ELINK[vfat][elink]
                 prbs_errors[oh_select][vfat][elink]["lpgbt"] = lpgbt
                 prbs_errors[oh_select][vfat][elink]["lpgbt_elink"] = elink_nr
                 err_str += "    ELINK %d (GBT: %d, Elink nr: %d): Nr. of PRBS errors = %d"%(elink, lpgbt, elink_nr, prbs_errors[oh_select][vfat][elink]["n_errors"])
